edccd6495b896e813b9573a61dd4b3f3e8bc926b
Benzel <benawhiting@gmail.com>
2025-03-03 17:07:01 -0800

    Add a menu by leveraging AnsiConsole

--- Program.cs
+++ Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Spectre.Console;
+
+string[] menuChoices = { "View Books", "Add Book", "Delete Book" };
+
+var choice = AnsiConsole.Prompt(
+    new SelectionPrompt<string>()
+    .Title("What do you want to do next?")
+    .AddChoices(menuChoices));

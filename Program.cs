d5466c2f3eabed9b6cdd12054b3f9bdd8a86504f
Benzel <benawhiting@gmail.com>
2025-03-11 17:26:55 -0700

    Add LibraryItem abstract class

--- Program.cs
+++ Program.cs
@@ -2,3 +2,31 @@
 
 UserInterface userInterface = new();
 userInterface.MainMenu();
+
+internal abstract class Animal
+{
+    public string Name { get; set; }
+    public int Age { get; set; }
+
+    protected Animal(string name, int age)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    // Abstract method that must be implemented by derived classes
+    public abstract void MakeSound();
+}
+
+internal class Dog : Animal
+{
+    public Dog(string name, int age) : base(name, age)
+    {
+
+    }
+
+    public override void MakeSound()
+    {
+        Console.WriteLine($"{Name} says: Woof!");
+    }
+}
